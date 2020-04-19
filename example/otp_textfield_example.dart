841833de5c5d1f24a6b0cea98b3fa5a95d3ca0fd
David <davidcobbina47@gmail.com>
2020-04-19 18:14:21 +0000

    otpTextfields package complete

---
+++ example/otp_textfield_example.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:otptextfield/otptextfield.dart';
+
+class ExamplePinEntry extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pin Entry Example"),
+      ),
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: OtpTextField(
+            numberOfFields: 5,
+            borderColor: Color(0xFF512DA8),
+            onSubmit: (String verificationCode){
+              //navigate to different screen code goes here
+              showDialog(
+                  context: context,
+                  builder: (context){
+                    return AlertDialog(
+                      title: Text("Verification Code"),
+                      content: Text('Code entered is $verificationCode'),
+                    );
+                  }
+              );
+            }, // end onSubmit
+          ), // end PinEntryTextField()
+        ), // end Padding()
+      ), // end Container()
+    );
+  }
+}
